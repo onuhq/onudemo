e4131d068af6db8ee1635461f368cf80186aaba2
Chine Ikoro <chinenye.ikoro@gmail.com>
2022-12-17 17:23:57 +0200

    Update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,9 +29,13 @@ function joinClassNames(...classes: Array<string | boolean | undefined>) {
   return classes.filter(Boolean).join(' ')
 }
 
-const PlayIcon = () => {
+interface IconProps {
+  classes: string;
+}
+
+const PlayIcon = ({ classes }: IconProps) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF" className="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF" className={classes}>
       <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
     </svg>
 
@@ -42,6 +46,33 @@ const XIcon = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#6b7280" className="w-6 h-6">
       <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clipRule="evenodd" />
+    </svg>
+  )
+}
+
+const CheckCircleIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#22c55e" className="w-10 h-10">
+      <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+    </svg>
+
+  )
+}
+
+const XCircleIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ef4444" className="w-10 h-10">
+      <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clipRule="evenodd" />
+    </svg>
+  )
+}
+
+
+
+const RefreshIcon = ({ classes }: IconProps) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={classes}>
+      <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" />
     </svg>
   )
 }
@@ -191,11 +222,11 @@ export default function Home() {
 
             </div>
             <div className='flex justify-end mb-6'>
-              <button onClick={clearScenario} className='px-3 py-2 bg-white border border-gray-400 rounded-lg text-gray-500 font-bold'>
-                Clear
+              <button onClick={clearScenario} className='px-3 py-2 bg-white border-2 border-gray-400 rounded-lg text-gray-500 font-bold flex flex-row items-center'>
+                Clear <RefreshIcon classes={"w-5 h-5 ml-2"} />
               </button>
-              <button disabled={runButtonDisabled} onClick={runScenario} className={joinClassNames(runButtonDisabled ? 'bg-gray-500' : 'bg-green-500', 'ml-5 px-5 py-2 rounded-lg text-white font-bold flex flex-row')}>
-                Run <PlayIcon />
+              <button disabled={runButtonDisabled} onClick={runScenario} className={joinClassNames(runButtonDisabled ? 'bg-gray-500' : 'bg-green-500', 'ml-5 px-3 py-2 rounded-lg text-white font-bold flex flex-row items-center')}>
+                Run <PlayIcon classes={"w-6 h-6 ml-1"} />
               </button>
             </div>
 
@@ -203,15 +234,17 @@ export default function Home() {
 
             <div>
               {scenario.map((scenarioStep, i) => {
+                const isSuccess = stepOutput[i] && !!stepOutput[i].stdout;
+                const isFailure = stepOutput[i] && !!stepOutput[i].stderr;
                 return (
                   <div className='w-full mb-6' key={`scenariostep${scenarioStep.id}`}>
-                    <div className='bg-gray-200 px-5 py-3 mb-3 rounded-lg'>
+                    <div className={joinClassNames(isFailure ? 'border-red-500' : isSuccess ? 'border-green-500' : 'border-gray-200', 'bg-gray-200 border-2 px-5 py-3 mb-3 rounded-lg')}>
                       <div className='flex flex-row w-full justify-between'>
 
                         <p className='font-bold mb-2 text-lg'>{scenarioStep.name}</p>
-                        <div onClick={() => removeAtId(scenarioStep.id)}>
+                        {!scenarioExecuted && <div onClick={() => removeAtId(scenarioStep.id)}>
                           <XIcon />
-                        </div>
+                        </div>}
 
                       </div>
 
@@ -226,15 +259,18 @@ export default function Home() {
                           )
                         })}
                       </div>
-
+                      <div className='w-full flex flex-row justify-end'>
+                        {isSuccess ? <CheckCircleIcon /> : isFailure ?
+                          <XCircleIcon /> : null}
+                      </div>
 
 
                     </div>
                     <div>
                       {/* Step output goes here */}
                       {stepOutput[i] && (
-                        <div className={joinClassNames(stepOutput[i].stderr ? 'bg-red-200' : 'bg-green-200', 'px-3 py-3 flex flex-col rounded-lg')}>
-                          <p className={joinClassNames(stepOutput[i].stderr ? 'text-red-800' : 'text-green-800', 'text-sm')}>Output:</p>
+                        <div className={joinClassNames(isFailure ? 'bg-red-200' : 'bg-green-200', 'px-3 py-3 flex flex-col rounded-lg')}>
+                          <p className={joinClassNames(isFailure ? 'text-red-800' : 'text-green-800', 'text-sm')}>Output:</p>
                           <pre className='font-mono text-xs text-gray-800'>{stepOutput[i].stdout ? stepOutput[i].stdout : stepOutput[i].stderr}</pre>
                         </div>
                       )}
