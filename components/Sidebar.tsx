6cc32e4acaf685ea1dcaf4e60ebfcb121806322c
Chine Ikoro <chinenye.ikoro@gmail.com>
2023-01-10 12:01:33 -0800

    Allow users to save scenarios

---
+++ components/Sidebar.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Sidebar = () => {
+  return (
+    <div className='bg-gray-300 w-72 flex flex-col pt-6'>
+      <Link href={'/'}><Image className='self-center cursor-pointer' src="/logo.png" alt="Onu Logo" width={150} height={50} /></Link>
+      <Link href="/scenarios"><div className='self-center mt-20 hover:bg-gray-400 w-full py-3 px-5 cursor-pointer'>
+        <p>My Scenarios</p>
+      </div></Link>
+
+    </div>
+  )
+}
+
+export default Sidebar
